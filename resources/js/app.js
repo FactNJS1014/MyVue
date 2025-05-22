830e8d32f0a9fcd4af7c59a2fb4f8ca28a6a4931
Natdanai <natda002@gmail.com>
2025-05-22 19:57:43 +0700

    create design vue ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import  {createApp} from 'vue';
+import App from './App.vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+const app = createApp(App);
+
+app.use(Antd);
+app.mount('#app');
